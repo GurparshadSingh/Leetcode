082ee6a032b01a58d2b6fadb862828fcb4594877
Gurparshad Singh <gurparshad0878@gmail.com>
2026-08-01 19:53:09 +0530

    Time: 2 ms (100%), Space: 46.6 MB (7.67%) - LeetHub

--- 0437-path-sum-iii/0437-path-sum-iii.java
+++ 0437-path-sum-iii/0437-path-sum-iii.java
@@ -15,6 +15,7 @@
  */
 class Solution {
     int ans = 0;
+    HashMap<Long, Integer> map = new HashMap<>();
 
     public void dfs(TreeNode root, long sum, int targetSum) {
         if (root == null) {
@@ -23,21 +24,19 @@ class Solution {
 
         sum += root.val;
 
-        if (targetSum == sum) {
-            ans += 1;
+        if (map.containsKey(sum - targetSum)) {
+            ans += map.get(sum - targetSum);
         }
+        map.put(sum, map.getOrDefault(sum, 0) + 1);
 
         dfs(root.left, sum, targetSum);
         dfs(root.right, sum, targetSum);
+        map.put(sum, map.get(sum) - 1);
     }
 
     public int pathSum(TreeNode root, int targetSum) {
-        if(root==null){
-            return 0;
-        }
-        dfs(root,0,targetSum);
-        pathSum(root.left,targetSum);
-        pathSum(root.right,targetSum);
+        map.put(0L, 1);
+        dfs(root, 0L, targetSum);
         return ans;
     }
 }
