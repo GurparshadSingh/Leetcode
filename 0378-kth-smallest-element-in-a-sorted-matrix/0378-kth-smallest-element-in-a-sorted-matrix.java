e9ab907190dfb4d48a2bf08000579f9116f99d10
Gurparshad Singh <gurparshad0878@gmail.com>
2026-08-12 20:19:06 +0530

    Time: 0 ms (100%), Space: 52.3 MB (5.54%) - LeetHub

--- 0378-kth-smallest-element-in-a-sorted-matrix/0378-kth-smallest-element-in-a-sorted-matrix.java
+++ 0378-kth-smallest-element-in-a-sorted-matrix/0378-kth-smallest-element-in-a-sorted-matrix.java
@@ -1,36 +1,43 @@
 class Solution {
-    class Pair {
-        int val;
-        int x;
-        int y;
+    public int kthSmallest(int[][] matrix, int k) {
+        int n = matrix.length;
 
-        Pair(int val, int x, int y) {
-            this.val = val;
-            this.x = x;
-            this.y = y;
+        int low = matrix[0][0];
+        int high = matrix[n - 1][n - 1];
+        while (low < high) {
+
+            int mid = low + (high - low) / 2;
+
+            int count = countLessEqual(matrix, mid);
+            if (count < k) {
+                low = mid + 1;
+            } else {
+                high = mid;
+            }
         }
+        return low;
     }
 
-    public int kthSmallest(int[][] matrix, int k) {
-        PriorityQueue<Pair> pq = new PriorityQueue<>(
-                (a, b) -> {
-                    return a.val - b.val;
-                });
+    private int countLessEqual(int[][] matrix, int mid) {
+        int n = matrix.length;
 
-        for (int i = 0; i < matrix.length; i++) {
-            pq.offer(new Pair(matrix[i][0], i, 0));
-        }
-        ArrayList<Integer> ans = new ArrayList<>();
+        int r = n - 1;
+        int c = 0;
 
-        while (!pq.isEmpty()) {
-            Pair curr = pq.poll();
+        int count = 0;
 
-            if (curr.y < matrix[0].length-1) {
-                pq.offer(new Pair(matrix[curr.x][curr.y + 1], curr.x, curr.y + 1));
+        while (r >= 0 && c < n) {
+            if (matrix[r][c] <= mid) {
+
+                count += r + 1;
+                c++;
+
+            } else {
+
+                r--;
+
             }
-
-            ans.add(curr.val);
         }
-        return ans.get(k - 1);
+        return count;
     }
 }
